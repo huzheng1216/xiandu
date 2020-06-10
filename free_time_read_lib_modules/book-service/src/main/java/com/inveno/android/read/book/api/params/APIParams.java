0671e1c37337202e269ab9a9011e763744141815
yunlong.yang <yunlong.yang@inveno.com>
2020-06-10 17:08:25 +0800

    Book-Service模块增加公共参数类

---
+++ free_time_read_lib_modules/book-service/src/main/java/com/inveno/android/read/book/api/params/APIParams.java
@@ -0,0 +1,13 @@
+package com.inveno.android.read.book.api.params;
+
+import java.util.LinkedHashMap;
+
+/**
+ * API参数参数辅助类，用来填充公共参数
+ * @作者 杨云龙
+ */
+public class APIParams {
+    public static LinkedHashMap<String,Object> fillCommonParam(LinkedHashMap<String,Object> paramMap) {
+        return paramMap;
+    }
+}
